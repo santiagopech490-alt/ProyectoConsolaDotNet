72859d2db6d157c765b27a7a67fcd7ae4510f9d8
SantiagoAsahel <santiagopech490@gmail.com>
2026-01-30 10:55:15 -0600

    feat: Initial project setup and core entities

---
+++ src/core/entities/Estudiante.cs
@@ -0,0 +1,10 @@
+namespace say_hello.src.core.entities
+{
+    public class Estudiante
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Cuatrimestre { get; set; }
+        public string Matricula { get; set; }
+    }
+}
